6df044ff4f247ac60718ade49194821a45f7e6f0
yoshihiko-a <yoshihiko-a@weseek.co.jp>
2024-06-11 22:31:17 +0900

    ex04 printrune version ok

--- go00/ex04/vendor/piscine/ex04.go
+++ go00/ex04/vendor/piscine/ex04.go
@@ -1,18 +1,20 @@
 package piscine
 
-import "syscall"
+import "ft"
 
 func PrintComb() {
-	fd := syscall.Stdout
 	for i := '0'; i <= '7'; i++ {
 		for j := i + 1; j <= '8'; j++ {
 			for k := j + 1; k <= '9'; k++ {
-				syscall.Write(fd, []byte{byte(i), byte(j), byte(k)})
+				ft.PrintRune(i)
+				ft.PrintRune(j)
+				ft.PrintRune(k)
 				if i != '7' {
-					syscall.Write(fd, []byte(", "))
+					ft.PrintRune(',')
+					ft.PrintRune(' ')
 				}
 			}
 		}
 	}
-	syscall.Write(fd, []byte("\n"))
+	ft.PrintRune('\n')
 }

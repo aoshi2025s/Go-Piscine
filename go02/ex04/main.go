6ae3d71674b0b3379120d6dfb39dd444b94ed74e
aoshi2025s <aoshi2025s@gmail.com>
2024-06-15 23:59:45 +0900

    ex04 ok

---
+++ go02/ex04/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Fibonacci(-42))
+	fmt.Println(piscine.Fibonacci(-1))
+	fmt.Println(piscine.Fibonacci(0))
+	fmt.Println(piscine.Fibonacci(1))
+	fmt.Println(piscine.Fibonacci(2))
+	fmt.Println(piscine.Fibonacci(3))
+	fmt.Println(piscine.Fibonacci(4))
+	fmt.Println(piscine.Fibonacci(5))
+	fmt.Println(piscine.Fibonacci(6))
+	fmt.Println(piscine.Fibonacci(7))
+	fmt.Println(piscine.Fibonacci(8))
+	fmt.Println(piscine.Fibonacci(9))
+	fmt.Println(piscine.Fibonacci(10))
+	fmt.Println(piscine.Fibonacci(11))
+	fmt.Println(piscine.Fibonacci(12))
+	fmt.Println(piscine.Fibonacci(13))
+	fmt.Println(piscine.Fibonacci(14))
+	fmt.Println(piscine.Fibonacci(15))
+	fmt.Println(piscine.Fibonacci(16))
+	fmt.Println(piscine.Fibonacci(17))
+	fmt.Println(piscine.Fibonacci(18))
+	fmt.Println(piscine.Fibonacci(19))
+	fmt.Println(piscine.Fibonacci(20))
+	fmt.Println(piscine.Fibonacci(21))
+	fmt.Println(piscine.Fibonacci(22))
+	fmt.Println(piscine.Fibonacci(23))
+	fmt.Println(piscine.Fibonacci(30))
+	fmt.Println(piscine.Fibonacci(35))
+	fmt.Println(piscine.Fibonacci(40))
+
+	fmt.Println(piscine.Fibonacci(43))
+	fmt.Println(piscine.Fibonacci(44))
+
+
+}

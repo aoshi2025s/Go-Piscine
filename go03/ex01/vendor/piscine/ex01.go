447ca560646b98865a1549a98a18afc70663e4e4
aoshi2025s <aoshi2025s@gmail.com>
2024-06-17 00:15:26 +0900

    fix: count runes length not string length

--- go03/ex01/vendor/piscine/ex01.go
+++ go03/ex01/vendor/piscine/ex01.go
@@ -2,15 +2,15 @@ package piscine
 
 func NRune(s string, n int) rune {
 	runes := []rune(s)
-	if n <= 0 || n > strLen(s) {
+	if n <= 0 || n > rLen(runes) {
 		return 0
 	}
 	return runes[n-1]
 }
 
-func strLen(s string) int {
+func rLen(r []rune) int {
 	c := 0
-	for range s {
+	for range r {
 		c++
 	}
 	return c

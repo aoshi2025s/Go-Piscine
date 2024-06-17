432819204487b6b5c49dd93f7d6a6a26fa2fb40e
aoshi2025s <aoshi2025s@gmail.com>
2024-06-18 00:22:31 +0900

    ex08 ok

---
+++ go03/ex08/vendor/piscine/ex08.go
@@ -0,0 +1,19 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	runes := []rune(s)
+	for i := range runes {
+		if runes[i] >= 'A' && runes[i] <= 'Z' {
+			continue
+		} else if runes[i] >= 'a' && runes[i] <= 'z' {
+			continue
+		} else if runes[i] == ' ' {
+			continue
+		} else if runes[i] >= '0' && runes[i] <= '9' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}

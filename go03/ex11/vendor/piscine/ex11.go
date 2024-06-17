e4797b21c0e54d5351e8d616986618fd3e2c0ddd
aoshi2025s <aoshi2025s@gmail.com>
2024-06-18 00:51:28 +0900

    ex11 fix

--- go03/ex11/vendor/piscine/ex11.go
+++ go03/ex11/vendor/piscine/ex11.go
@@ -3,7 +3,7 @@ package piscine
 func IsLower(s string) bool {
 	runes := []rune(s)
 	for i := range runes {
-		if runes[i] < 'a' || runes[i] >= 'z' {
+		if runes[i] < 'a' || runes[i] > 'z' {
 			return false
 		}
 	}

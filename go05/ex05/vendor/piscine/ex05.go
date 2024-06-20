7d8be7c46226426a8be2f68cbb8c641c95d56f1f
aoshi2025s <aoshi2025s@gmail.com>
2024-06-21 04:52:09 +0900

    ex05 ok

--- go05/ex05/vendor/piscine/ex05.go
+++ go05/ex05/vendor/piscine/ex05.go
@@ -1,9 +1,46 @@
 package piscine
 
 func ConvertBase(nbr, baseFrom, baseTo string) string {
-	// nbr: A string representing a numberic value in a base
-	// baseFrom: A string representing the base nbr it's using.
-	// baseTo: A string representing the base nbr should be represented in the re-turned value.
-	// Only valid bases will be tested
-	// Negative numbers will not be tested.
+	
+	// baseFrom -> int
+	decimal := AtoiBase(nbr, baseFrom)
+
+	// int -> baseTo
+	baseLen := strLen(baseTo)
+	result := ""
+	for decimal > 0 {
+		result = string(baseTo[decimal % baseLen]) + result
+		decimal /= baseLen
+	}
+	return result
+}
+
+func strLen(s string) int {
+	c := 0
+	for range s {
+		c++
+	}
+	return c
+}
+
+func AtoiBase(s string, base string) int {
+	result := 0
+	for _, r := range s {
+		value := sToNumInBase(r, base)
+		if value < 0 {
+			return result
+		}
+		result = result * strLen(base) + value
+	}
+	return result
+}
+
+func sToNumInBase(r rune, base string) int {
+	index := 0
+	for ; index < strLen(base); index++ {
+		if r == rune(base[index]) {
+			return index
+		}
+	}
+	return -1
 }

5ba2f59e8b961df238ebc716c353ffe451a445a7
aoshi2025s <aoshi2025s@gmail.com>
2024-06-20 22:39:01 +0900

    ex15 ok

--- go03/ex15/vendor/piscine/ex15.go
+++ go03/ex15/vendor/piscine/ex15.go
@@ -1,4 +1,20 @@
 package piscine
 
 func Join(strs []string, sep string) string {
+	if strsLen(strs) == 0 {
+		return ""
+	}
+	result := strs[0]
+	for _, str := range strs[1:] {
+		result += sep + str
+	}
+	return result
+}
+
+func strsLen(strs []string) int {
+	c := 0
+	for range strs {
+		c++
+	}
+	return c
 }

41f09e381df1923722eee0c9333a6bff81ef1a2e
aoshi2025s <aoshi2025s@gmail.com>
2024-06-21 11:45:53 +0900

    hosoku

--- go00/ex06/vendor/piscine/ex06.go
+++ go00/ex06/vendor/piscine/ex06.go
@@ -31,3 +31,47 @@ func printCombR(combs []int, n int, index int) {
 		printCombR(combs, n, index+1)
 	}
 }
+
+/*
+PrintCombN(3)
+printCombR(combs[:3], 3, 0)
+for i := 0; i <= 7; i++ {
+	combs[0] = 0
+	printCombR(combs, 3, 1)
+	for i := 1; i <= 8; i++ {
+		combs[1] = 1
+		printCombR(combs, 3, 2)
+		for i := 2; i <= 9; i++ {
+			combs[2] = 2
+			printCombR(combs, 3, 3)
+			// 012,
+			combs[2] = 3
+			printCombR(combs, 3, 3)
+			// 013,
+			combs[2] = 4
+			printCombR(combs, 3, 3)
+			// 014,
+			.
+			.
+			.
+		}
+	}
+}
+
+PrintCombN(1)
+printCombR(combs[:1], 1, 0)
+for i := 0; i <= 9; i++ {
+	combs[0] = 0
+	printCombR(combs, 1, 1)
+	// 0, 
+	combs[0] = 1
+	printCombR(combs, 1, 1)
+	// 1, 
+	.
+	.
+	.
+	combs[0] = 9
+	printCombR(combs, 1, 1)
+	// 9
+}
+*/

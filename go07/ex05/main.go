5cf63eaca154d963953dcc7ec8ffe7273b00bd57
aoshi2025s <aoshi2025s@gmail.com>
2024-06-28 04:08:57 +0900

    ex05 ok

---
+++ go07/ex05/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	//test()
+
+	args := os.Args
+	params := args[1:]
+	if piscine.IsValidInput(params) == false {
+		return
+	}
+
+	value1, err1 := piscine.Atoi(params[0])
+	value2, err2 := piscine.Atoi(params[2])
+	op := params[1]
+
+	// overflow
+	if err1 == false || err2 == false {
+		return
+	}
+
+	if value2 == 0 && op[0] == '/' {
+		fmt.Println("No division by 0")
+		return
+	} else if value2 == 0 && op[0] == '%' {
+		fmt.Println("No modulo by 0")
+		return
+	}
+	
+	result, err := piscine.CalcuDoop(value1, value2, op)
+	if err == false {
+		return
+	}
+	fmt.Println(result)
+}
+
+func test() {
+	var params []string
+	success := "\033[32m"  // 緑色の開始
+	failure := "\033[31m"  // 赤色の開始
+	reset := "\033[0m"     // 色をリセット
+
+	if !piscine.IsValidInput(params) {
+		fmt.Println(success + "no input test ok" + reset)
+	} else {
+		fmt.Println(failure + "no input test failed" + reset)
+	}
+
+	params = []string{"hello", "+", "1"}
+	if !piscine.IsValidInput(params) {
+		fmt.Println(success + "not valid input test ok" + reset)
+	} else {
+		fmt.Println(failure + "not valid input test failed" + reset)
+	}
+
+	params = []string{"1", "p", "1"}
+	if !piscine.IsValidInput(params) {
+		fmt.Println(success + "not valid input test ok" + reset)
+	} else {
+		fmt.Println(failure + "not valid input test failed" + reset)
+	}
+
+	result, err := piscine.CalcuDoop(1, 0, "/")
+	if !err {
+		fmt.Println(success + "division 0 test ok" + reset)
+	} else {
+		fmt.Println(failure + "division 0 test failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(1, 0, "%")
+	if !err {
+		fmt.Println(success + "modulo 0 test ok" + reset)
+	} else {
+		fmt.Println(failure + "modulo 0 test failed" + reset)
+	}
+
+	result, err = piscine.Atoi("9223372036854775807")
+	if err && result == 9223372036854775807 {
+		fmt.Println(success + "Atoi function test ok" + reset)
+	} else {
+		fmt.Println(failure + "Atoi function test failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(9223372036854775807, 1, "+")
+	if !err {
+		fmt.Println(success + "+ operator overflow test ok" + reset)
+	} else {
+		fmt.Println(failure + "+ operator overflow test failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(-9223372036854775807, 3, "-")
+	if !err {
+		fmt.Println(success + "- operator overflow test ok" + reset)
+	} else {
+		fmt.Println(failure + "- operator overflow test failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(9223372036854775807, 3, "*")
+	if !err {
+		fmt.Println(success + "* operator overflow test ok" + reset)
+	} else {
+		fmt.Println(failure + "* operator overflow test failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(-9223372036854775808, -1, "/")
+	if !err {
+		fmt.Println(success + "/ operator overflow test ok" + reset)
+	} else {
+		fmt.Println(failure + "/ operator overflow test failed" + reset)
+	}
+
+	// basic test
+	result, err = piscine.CalcuDoop(1, 1, "+")
+	if err && result == 2 {
+		fmt.Println(success + "basic test1 ok" + reset)
+	} else {
+		fmt.Println(failure + "basic test1 failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(1, 1, "*")
+	if err && result == 1 {
+		fmt.Println(success + "basic test2 ok" + reset)
+	} else {
+		fmt.Println(failure + "basic test2 failed" + reset)
+	}
+
+	result, err = piscine.CalcuDoop(1, -1, "*")
+	if err && result == -1 {
+		fmt.Println(success + "basic test3 ok" + reset)
+	} else {
+		fmt.Println(failure + "basic test3 failed" + reset)
+	}
+}
---
+++ go07/ex05/vendor/piscine/doop.go
@@ -0,0 +1,196 @@
+package piscine
+
+func CalcuDoop(v1, v2 int, op string) (int, bool) {
+	if op[0] == '+' {
+		if isSafeAdd(v1, v2) == false {
+			return 0, false
+		}
+		return v1 + v2, true
+	}
+	if op[0] == '-' {
+		if v1 < getMinInt() + v2 {
+			return 0, false
+		}
+		return v1 - v2, true
+	}
+	if op[0] == '*' {
+		if isSafeMultiply(v1, v2) == false {
+			return 0, false
+		}
+		return v1 * v2, true
+	}
+	if op[0] == '/' {
+		if isSafeDivide(v1, v2) == false {
+			return 0, false
+		}
+		return v1 / v2, true
+	}
+	if op[0] == '%' {
+		if v2 == 0 {
+			return 0, false
+		}
+		return v1 % v2, true
+	}
+	return 0, false
+}
+
+func Atoi(s string) (int, bool) {
+	result := 0
+	minus := 1
+
+	if s[0] == '-' {
+		minus = -1
+		s = s[1:]
+	}
+
+	for _, r := range s {
+		if IsOverFlow(result * minus, int(r - '0')) == true {
+			return result * minus, false
+		}
+		result = result * 10 + int(r - '0')
+	}
+	return result * minus, true
+}
+
+func getMaxInt() int {
+	if ^uint(0) >> 32 == 0 {
+		// 32bit
+		return 1 << 31 - 1 // 2147483647
+	} else {
+		// 64bit
+		return 1 << 63 - 1 // 9223372036854775807
+	}
+}
+
+func getMinInt() int {
+	if ^uint(0) >> 32 == 0 {
+		// 32 bit
+		return -1 << 31 //-2147483648
+	} else {
+		// 64 bit
+		return -1 << 63 // -9223372036854775808
+	}
+}
+
+
+func IsOverFlow(n, p int) bool {
+	maxInt := getMaxInt()
+	minInt := getMinInt()
+
+	if n > maxInt / 10 {
+		return true
+	} else if n == maxInt / 10 && p > maxInt % 10 {
+		return true
+	}
+	if n < minInt / 10 {
+		return true
+	} else if n == minInt / 10 && p >  -(minInt % 10) {
+		return true
+	}
+	return false
+}
+
+
+func IsValidInput(params []string) bool {
+	if elemLen(params) != 3 {
+		return false
+	}
+	if isDigit(params[0]) == false || isDigit(params[2]) == false {
+		return false
+	}
+	if isDoop(params[1]) == false {
+		return false
+	}
+	return true
+}
+
+func isDoop(s string) bool {
+	if strLen(s) > 1 {
+		return false
+	}
+	runes := []rune(s)
+	if runes[0] == '*' || runes[0] == '/' || runes[0] == '%' {
+		return true
+	} else if runes[0] == '+' || runes[0] == '-' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isSafeAdd(v1, v2 int) bool {
+	maxInt := getMaxInt()
+	minInt := getMinInt()
+
+	if v1 > 0 && v2 > 0 && v2 > maxInt - v1 {
+		return false
+	}
+	if v1 < 0 && v2 < 0 && v2 < minInt - v1 {
+		return false
+	}
+	return true
+}
+
+func isSafeMultiply(v1, v2 int) bool {
+	maxInt := getMaxInt()
+	minInt := getMinInt()
+	
+	// INT_MIN * (-1)
+	if (v1 == minInt && v2 == -1) || (v2 == minInt && v1 == -1) {
+		return false
+	}
+	if v1 > 0 && v2 > 0 && v1 > maxInt / v2 {
+		return false
+	}
+	if v1 > 0 && v2 < 0 && v2 < minInt / v1 {
+		return false
+	}
+	if v1 < 0 && v2 > 0 && v1 < minInt / v2 {
+		return false
+	}
+	if v1 < 0 && v2 < 0 && v1 < maxInt / v2 {
+		return false
+	}
+	return true
+}
+
+func isSafeDivide(v1, v2 int) bool {
+	minInt := getMinInt()
+
+	if v2 == 0 {
+		return false
+	}
+
+	if v1 == minInt && v2 == -1 {
+		return false
+	}
+	return true
+}
+
+func strLen(s string) int {
+	c := 0
+	for range s {
+		c++
+	}
+	return c
+}
+
+func isDigit(s string) bool {
+	if s[0] == '-' {
+		s = s[1:]
+	}
+	for _, r := range s {
+		if r < '0' || r > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func elemLen(strs []string) int {
+	count := 0
+	for range strs {
+		count++
+	}
+	return count
+}

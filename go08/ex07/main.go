44b6b2f19f5a1869527a0e5271a6236ad49e14de
aoshi2025s <aoshi2025s@gmail.com>
2024-06-28 06:56:00 +0900

    update main

--- go08/ex07/main.go
+++ go08/ex07/main.go
@@ -6,5 +6,19 @@ import (
 )
 
 func main() {
+	fmt.Println("count of active bits")
 	fmt.Println(piscine.ActiveBits(7))
+	fmt.Println(piscine.ActiveBits(4))
+	fmt.Println(piscine.ActiveBits(133))
+	fmt.Println(piscine.ActiveBits(991199))
+	fmt.Println(piscine.ActiveBits(214746847))
+	fmt.Println(piscine.ActiveBits(57))
+	
+	fmt.Println("bit of value")
+	fmt.Printf("%b\n", 7)
+	fmt.Printf("%b\n", 4)
+	fmt.Printf("%b\n", 133)
+	fmt.Printf("%b\n", 991199)
+	fmt.Printf("%b\n", 214746847)
+	fmt.Printf("%b\n", 57)
 }

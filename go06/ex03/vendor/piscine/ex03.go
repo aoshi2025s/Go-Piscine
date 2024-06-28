d4d3ae1f349557ec24cc4ce857d9eea3beaf977b
aoshi2025s <aoshi2025s@gmail.com>
2024-06-28 17:22:35 +0900

    ex03 update

--- go06/ex03/vendor/piscine/ex03.go
+++ go06/ex03/vendor/piscine/ex03.go
@@ -17,6 +17,30 @@ func CatWithStdin() {
 }
 
 func CatWithFile(fileName string) {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		PrintStr("ERROR: " + err.Error() + "\n")
+		return
+	}
+	PrintStr(string(content))
+}
+
+func PrintStr(str string) {
+	for _, r := range str {
+		ft.PrintRune(r)
+	}
+}
+
+func CountArgs(args []string) int {
+	c := 0
+	for range args {
+		c++
+	}
+	return c
+}
+
+/*
+func CatWithFile(fileName string) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		PrintStr("ERROR: " + err.Error() + "\n")
@@ -34,17 +58,4 @@ func CatWithFile(fileName string) {
 		ft.PrintRune(rune(buf[0]))
 	}
 }
-
-func PrintStr(str string) {
-	for _, r := range str {
-		ft.PrintRune(r)
-	}
-}
-
-func CountArgs(args []string) int {
-	c := 0
-	for range args {
-		c++
-	}
-	return c
-}
+*/

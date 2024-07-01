533a6463aaf77d4bca30bd5ad382478b16a9b87b
aoshi2025s <aoshi2025s@gmail.com>
2024-07-01 13:53:18 +0900

    ex01

--- go07/ex01/main.go
+++ go07/ex01/main.go
@@ -10,4 +10,14 @@ func main() {
 	result := piscine.Map(piscine.IsPrime, a)
 	fmt.Println(a)
 	fmt.Println(result)
+
+	b := []int{57, 13, 17, 19, 23, 2147483647}
+	result = piscine.Map(piscine.IsPrime, b)
+	fmt.Println(b)
+	fmt.Println(result)
+
+	c := []int{-9223372036854775808, 9223372036854775807}
+	result = piscine.Map(piscine.IsPrime, c)
+	fmt.Println(c)
+	fmt.Println(result)
 }
--- go07/ex01/vendor/piscine/map.go
+++ go07/ex01/vendor/piscine/map.go
@@ -16,9 +16,6 @@ func IsPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	if n == 2 {
-		return true
-	}
 	for i := 2; i <= n / i; i++ {
 		if n % i == 0 {
 			return false 

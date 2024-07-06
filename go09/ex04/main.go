bc2e51705e16553d87ff0f6250b26b657b168372
aoshi2025s <aoshi2025s@gmail.com>
2024-07-06 21:09:25 +0900

    ex04 ok

--- go09/ex04/main.go
+++ go09/ex04/main.go
@@ -5,11 +5,29 @@ import (
 	"piscine"
 )
 
-func main() {
-	
-	link := &piscine.List{}
+type List = piscine.List
+type Node = piscine.NodeL
 
-	piscine.List(link, "Hello")
-	piscine.List(link, "there")
-	piscine.List(link, "how are you")
+func PrintList(l *List) {
+	link := l.Head
+	for link != nil {
+		fmt.Print(link.Data, " -> ")
+		link = link.Next
+	}
+	fmt.Println(nil)
+}
+
+func main() {
+	link := &List{}	
+
+	piscine.ListPushBack(link, "I")
+	piscine.ListPushBack(link, 1)
+	piscine.ListPushBack(link, "something")
+	piscine.ListPushBack(link, 2)
+
+	fmt.Println("-------list------")
+	PrintList(link)
+	piscine.ListClear(link)
+	fmt.Println("-------updated list------")
+	PrintList(link)
 }
---
+++ go09/ex04/vendor/piscine/listclear.go
@@ -0,0 +1,27 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListClear(l *List) {
+	l.Head = nil
+	l.Tail = nil
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+		return
+	}
+	l.Tail.Next = n
+	l.Tail = n
+}

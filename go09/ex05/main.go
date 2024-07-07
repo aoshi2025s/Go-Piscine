f6f990f6ddbda72cff1aedba8314b9739506d4f5
aoshi2025s <aoshi2025s@gmail.com>
2024-07-07 18:33:01 +0900

    ex05 ok

--- go09/ex05/main.go
+++ go09/ex05/main.go
@@ -9,7 +9,13 @@ func main() {
 	
 	link := &piscine.List{}
 
-	piscine.List(link, "Hello")
-	piscine.List(link, "there")
-	piscine.List(link, "how are you")
+	piscine.ListPushBack(link, "Hello")
+	piscine.ListPushBack(link, "there")
+	piscine.ListPushBack(link, "how are you")
+	piscine.ListPushBack(link, 1)
+
+	fmt.Println(piscine.ListAt(link.Head, 3).Data)
+	fmt.Println(piscine.ListAt(link.Head, 1).Data)
+	fmt.Println(piscine.ListAt(link.Head, 7))
+	// fmt.Println(piscine.ListAt(link.Head, 7).Data) -> segv
 }
---
+++ go09/ex05/vendor/piscine/listat.go
@@ -0,0 +1,33 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+		return
+	}
+	l.Tail.Next = n
+	l.Tail = n
+}
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	current := l
+	for i := 0; i < pos; i++ {
+		if current == nil {
+			return nil
+		}
+		current = current.Next
+	}
+	return current
+}

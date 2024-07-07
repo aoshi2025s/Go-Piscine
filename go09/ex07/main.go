c3192893ec8a5373bfcda3a44fb78e0fc63cca11
aoshi2025s <aoshi2025s@gmail.com>
2024-07-07 19:49:07 +0900

    ex07 ok

--- go09/ex07/main.go
+++ go09/ex07/main.go
@@ -9,7 +9,24 @@ func main() {
 	
 	link := &piscine.List{}
 
-	piscine.List(link, "Hello")
-	piscine.List(link, "there")
-	piscine.List(link, "how are you")
+	piscine.ListPushBack(link, 1)
+	piscine.ListPushBack(link, 2)
+	piscine.ListPushBack(link, 3)
+	piscine.ListPushBack(link, 5)
+
+	it := link.Head
+	for it != nil {
+		fmt.Println(it.Data)
+		it = it.Next
+	}
+
+	piscine.ListForEach(link, piscine.Add2_node)
+	// piscine.ListForEach(link, piscine.Subtract3_node)
+	fmt.Println("after implement ListForEach")
+
+	it = link.Head
+	for it != nil {
+		fmt.Println(it.Data)
+		it = it.Next
+	}
 }
---
+++ go09/ex07/vendor/piscine/listforeach.go
@@ -0,0 +1,47 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListForEach(l *List, f func(*NodeL)) {
+	curr := l.Head
+	for curr != nil {
+		f(curr)
+		curr = curr.Next
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	}
+	l.Tail.Next = n
+	l.Tail = n
+}

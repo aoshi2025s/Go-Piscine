c8008f3707f223a779fceebd84193d02f963ebf3
aoshi2025s <aoshi2025s@gmail.com>
2024-07-08 11:49:44 +0900

    ex11 ok

--- go09/ex11/main.go
+++ go09/ex11/main.go
@@ -5,11 +5,35 @@ import (
 	"piscine"
 )
 
+func PrintList(l *piscine.List) {
+	it := l.Head
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+	fmt.Print(nil, "\n")
+}
+
 func main() {
 	
 	link := &piscine.List{}
+	link2 := &piscine.List{}
 
-	piscine.List(link, "Hello")
-	piscine.List(link, "there")
-	piscine.List(link, "how are you")
+	piscine.ListPushBack(link, "a")
+	piscine.ListPushBack(link, "b")
+	piscine.ListPushBack(link, "c")
+	piscine.ListPushBack(link, "d")
+	fmt.Println("---------first list----------")
+	PrintList(link)
+
+	piscine.ListPushBack(link2, "e")
+	piscine.ListPushBack(link2, "f")
+	piscine.ListPushBack(link2, "g")
+	piscine.ListPushBack(link2, "h")
+	fmt.Println("---------second list---------")
+	PrintList(link2)
+
+	fmt.Println("---------merged list---------")
+	piscine.ListMerge(link, link2)
+	PrintList(link)
 }
---
+++ go09/ex11/vendor/piscine/listmerge.go
@@ -0,0 +1,26 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListMerge(l1 *List, l2 *List) {
+	l1.Tail.Next = l2.Head
+}
+
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+		return
+	}
+	l.Tail.Next = n
+	l.Tail = n
+}

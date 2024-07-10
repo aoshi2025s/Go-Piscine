37d289092107c88dab3c11700a8c0aa6afa4f537
aoshi2025s <aoshi2025s@gmail.com>
2024-07-10 11:07:28 +0900

    ex04 ok

--- go10/ex04/main.go
+++ go10/ex04/main.go
@@ -5,13 +5,50 @@ import (
 	"piscine"
 )
 
+const (
+	blue = "\033[34m"
+	green = "\033[32m"
+	pink  = "\033[35m"
+	reset = "\033[0m"
+)
+
+func VisualizeBTree(root *piscine.TreeNode, prefix string, isLeft bool) {
+	if root != nil {
+		if root.Parent == nil {
+			fmt.Printf("%s%s%s\n", pink, root.Data, reset)
+		} else {
+			if isLeft {
+				fmt.Printf("%s%s├── %s%s%s%s\n", green,  prefix, reset,  pink, root.Data, reset)
+			} else {
+				fmt.Printf("%s%s└── %s%s%s%s\n", green, prefix, reset, pink, root.Data, reset)
+			}
+		}
+
+		newPrefix := prefix
+		if root.Parent != nil {
+			if isLeft {
+				newPrefix += "│   "
+			} else {
+				newPrefix += "    "
+			}
+		}
+
+		VisualizeBTree(root.Left, newPrefix, true)
+		VisualizeBTree(root.Right, newPrefix, false)
+	}
+}
+
 func main() {
 
 	root := &piscine.TreeNode{Data: "4"}
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	fmt.Println(piscine.BTreeLevelCount(root))
+	fmt.Printf("%s================================%s\n", pink, reset)
+	VisualizeBTree(root, "", false)
+	fmt.Printf("%s================================%s\n", pink, reset)
+	fmt.Printf("%scount level%s %d\n", blue, reset, piscine.BTreeLevelCount(root))
+	fmt.Printf("%s================================%s\n", pink, reset)
 
 
 	root = &piscine.TreeNode{Data: "1"}
@@ -35,7 +72,11 @@ func main() {
 	piscine.BTreeInsertData(root, "19")
 	piscine.BTreeInsertData(root, "20")
 	piscine.BTreeInsertData(root, "21")
-	fmt.Println(piscine.BTreeLevelCount(root))
+	fmt.Printf("%s================================%s\n", pink, reset)
+	VisualizeBTree(root, "", false)
+	fmt.Printf("%s================================%s\n", pink, reset)
+	fmt.Printf("%scount level%s %d\n", blue, reset, piscine.BTreeLevelCount(root))
+	fmt.Printf("%s================================%s\n", pink, reset)
 
 	// TODO: visualize BTree
 }
